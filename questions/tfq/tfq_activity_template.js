bef941f3cd063314fde394e5ba0c6169b1709927
Sergio <sergio@perceptum.nl>
2013-07-18 13:06:27 +0200

    Refactor true/false exercise for customizing answers

--- questions/tfq/tfq_activity_template.js
+++ questions/tfq/tfq_activity_template.js
@@ -3,25 +3,71 @@ var activity = [
   '<script>generateTrueFalseQuestion();</script>',
 ];
 
-var tfq_activity = [
-  { 
-    type: 'text',
-    text: 'YES',
-    concepts: [{
+//var tfq_activity = [
+//  { 
+//    type: 'text',
+//    text: 'YES',
+//    concepts: [{
+//      type: 'text',
+//      text: 'Are you human?'
+//    },{
+//      type: 'image',
+//      text: 'Is this a dog?',
+//      image: 'http://eofdreams.com/data_images/dreams/dog/dog-01.jpg',
+//    }]
+//  },
+//  {
+//    type: 'text',
+//    text: 'NO',
+//    concepts: [{
+//      type: 'text',
+//      text: 'Are you a pet?',
+//    }]
+//  }
+//]
+
+var tfq_activity = {
+  randomize: true,
+  questionsList: [
+    { 
       type: 'text',
-      text: 'Are you human?'
-    },{
+      text: 'Are you human?',
+      correctAnswer: 1,
+      answerMaybe: true,
+      answers: [{
+        type: 'text',
+        text: 'Yes',
+      },{
+        type: 'text',
+        text: 'No',
+      }]
+    },
+    {
       type: 'image',
       text: 'Is this a dog?',
       image: 'http://eofdreams.com/data_images/dreams/dog/dog-01.jpg',
-    }]
-  },
-  {
-    type: 'text',
-    text: 'NO',
-    concepts: [{
+      correctAnswer: 0,
+      answerMaybe: true,
+      answers: [{
+        type: 'text',
+        text: 'YES',
+      },{
+        type: 'text',
+        text: 'NO',
+      }]
+    },
+    {
       type: 'text',
       text: 'Are you a pet?',
-    }]
-  }
-]
+      correctAnswer: 1,
+      answerMaybe: true,
+      answers: [{
+        type: 'text',
+        text: 'YES',
+      },{
+        type: 'text',
+        text: 'NO',
+      }]
+    }
+  ]
+}
